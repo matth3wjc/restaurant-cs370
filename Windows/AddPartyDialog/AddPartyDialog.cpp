5a66be6ee47d49c8895f66fb486f23b5a936e1d2
matth3wjc <48262000+matth3wjc@users.noreply.github.com>
2020-11-09 02:29:33 -0800

    Updated AddPartyDialog to submit party data
    Added functionality to create a new party with the information provided by the user. Input validation and signalling still not completed. Need to test this a bit before committing to main but should be good.

--- Windows/AddPartyDialog/AddPartyDialog.cpp
+++ Windows/AddPartyDialog/AddPartyDialog.cpp
@@ -17,8 +17,17 @@ AddPartyDialog::~AddPartyDialog()
 
 void AddPartyDialog::on_confirmButton_clicked()
 {
+    //input validation still required
+
     QString party_name = ui->input_1->toPlainText();
-    QString party_nbr = ui->input_2->toPlainText();
+    //QString party_nbr = ui->input_2->toPlainText();
+    bool flag;
+    int party_nbr = (ui->input_2->toPlainText()).toInt(&flag);
+    if(!flag)
+    {
+        ui->warning->setText("NaN");
+        this->close();
+    }
 
     if (party_name == NULL || party_nbr == NULL)
     {
@@ -26,7 +35,7 @@ void AddPartyDialog::on_confirmButton_clicked()
     }
     else
     {
-        //submit information to DB
+        Party *&newParty = new Party(party_name, party_nbr);
         this->close();
     }
 }
