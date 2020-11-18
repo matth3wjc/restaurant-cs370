1a452532594cbfff996c8b92b2a3c53a4a425b4c
Ellendren <54527908+Ellendren@users.noreply.github.com>
2020-11-18 15:13:56 -0800

    Update Updater.cpp

--- Updaters/Updater.cpp
+++ Updaters/Updater.cpp
@@ -1,7 +1,9 @@
 #include "Updater.h"
+#include <QUrl>
 
 Updater::Updater(QObject *parent)
     :QObject(parent){
+    manager = new QNetworkAccessManager(this);
     connect(manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(replyFinished(QNetworkReply*)));
 }
 
@@ -13,7 +15,10 @@ Updater::~Updater(){
 void Updater::replyFinished(QNetworkReply *reply)
 {
     if(reply->error() == QNetworkReply::NoError){
-        emit ok();
+        if (reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt() == 200)
+            emit ok(reply->readAll().toInt());
+        else
+            emit ok();
     }
     else
         emit failure(reply->errorString());
