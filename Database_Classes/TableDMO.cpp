d50c35071c5766753487d1b8be3a2b6e8dc53daf
Ellendren <54527908+Ellendren@users.noreply.github.com>
2020-11-29 19:53:15 -0800

    Update TableDMO.cpp

--- Database_Classes/TableDMO.cpp
+++ Database_Classes/TableDMO.cpp
@@ -8,6 +8,33 @@ TableDMO::TableDMO(){
     cachedNbr = false;
     cachedSeats = false;
     cachedStatus = false;
+    cachedName = false;
+    cachedSize = false;
+    cachedId = false;
+}
+
+std::string PartyDMO::_name() {
+    if (cachedName)
+        return name;
+    cachedName = true;
+    name = valueForStringAttribute("p_name");
+    return name;
+}
+
+int PartyDMO::_size() {
+    if (cachedSize)
+        return size;
+    cachedSize = true;
+    size = valueForIntegerAttribute("p_size");
+    return size;
+}
+
+int PartyDMO::_id() {
+    if (cachedId)
+        return id;
+    cachedId = true;
+    id = valueForIntegerAttribute("p_id ");
+    return id;
 }
 
 std::string TableDMO::_status(){
