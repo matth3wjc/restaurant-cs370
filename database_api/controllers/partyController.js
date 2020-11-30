2f874b063426b90b2b03232d94c914be49595067
Ellendren <54527908+Ellendren@users.noreply.github.com>
2020-11-30 14:37:49 -0800

    Update partyController.js

--- database_api/controllers/partyController.js
+++ database_api/controllers/partyController.js
@@ -16,7 +16,7 @@ class partyController{
                     reject(`Error querying CHP.test: ${err}`);
                 }
 
-                ctx.body = res;
+                ctx.body = res[0].new_id;
                 ctx.status = 200;
                 
                 resolve();
